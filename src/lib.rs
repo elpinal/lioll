7f331fca8acd9e76596f32772b25e3ffab1b1173
elpinal <elpinal@users.noreply.github.com>
2017-12-17 23:31:05 +0900

    Add Lexer

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,21 @@
 //! The Lioll project.
 #![warn(missing_docs)]
 
+use std::io;
+use std::io::Read;
+use std::iter::Peekable;
+
 /// Represents tokens.
 enum Token {
     String(String),
     Number(isize),
     LBrack,
     RBrack,
+}
+
+/// A lexer.
+struct Lexer<R: Read + Sized> {
+    bytes: Peekable<io::Bytes<R>>,
 }
 
 #[cfg(test)]
