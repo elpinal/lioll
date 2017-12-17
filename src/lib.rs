ed4258a33de451bbd105abbc8c99e556a587ff6b
elpinal <elpinal@users.noreply.github.com>
2017-12-17 23:34:29 +0900

    Introduce LexError

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@
 use std::io;
 use std::io::Read;
 use std::iter::Peekable;
+use std::string::FromUtf8Error;
 
 /// Represents tokens.
 enum Token {
@@ -25,6 +26,32 @@ where
     /// Creates a new lexer.
     pub fn new(r: R) -> Lexer<R> {
         Lexer { bytes: r.bytes().peekable() }
+    }
+}
+
+#[derive(Debug)]
+pub enum LexError {
+    /// Error on IO.
+    IO(io::Error),
+    /// Invalid UTF-8 error.
+    Utf8(FromUtf8Error),
+    /// An error that indicates "string literal is not terminated."
+    Terminate,
+    /// End of file.
+    EOF,
+    /// Illegal byte.
+    Illegal,
+}
+
+impl From<io::Error> for LexError {
+    fn from(e: io::Error) -> LexError {
+        LexError::IO(e)
+    }
+}
+
+impl From<FromUtf8Error> for LexError {
+    fn from(e: FromUtf8Error) -> LexError {
+        LexError::Utf8(e)
     }
 }
 
